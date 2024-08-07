4957ab27ef00c1983daa384c096a9d820220019d
VeselinD22 <vesko.dimov2002@abv.bg>
2024-08-07 14:09:37 +0300

    Work on Modbus RTU

--- offgrid/src/bin/morse-home.rs
+++ offgrid/src/bin/morse-home.rs
@@ -1,4 +1,5 @@
 use morse::agent;
+#[allow(unused_imports)]
 use offgrid::register_types as rt;
 
 #[tokio::main(flavor = "current_thread")]
@@ -6,6 +7,5 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     log4rs::init_file("config/log4rs.yaml", Default::default()).unwrap();
     log::info!("Starting Morse");
     agent::Agent::run_from_yaml("config/morse_home.yaml").await?;
-    // log::info!("{:#?}", agent.config);
     Ok(())
 }
